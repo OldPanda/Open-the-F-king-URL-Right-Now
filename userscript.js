8333c5ae31f7860a3660bfda90fe4715fc4bef2f
OldPanda <me@old-panda.com>
2020-11-02 22:59:13 -0800

    Fix redirect from Zhipu

--- userscript.js
+++ userscript.js
@@ -11,7 +11,7 @@
 // @match          https://mp.weixin.qq.com/*
 // @match          http://redir.yy.duowan.com/warning.php?url=*
 // @match          https://weixin110.qq.com/cgi-bin/mmspamsupport-bin/newredirectconfirmcgi*
-// @version        0.6.3
+// @version        0.6.4
 // @run-at         document-idle
 // @namespace      https://old-panda.com/
 // @require        https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js
@@ -96,7 +96,6 @@
     for (let value of urls) {
       let urlStr = $.trim(value[0]);
       for (let url of splitMultiURLs(urlStr)) {
-        console.log(`URL: ${url}`);
         if (!url || replaced.has(url) || url.includes("localhost") || url.includes("127.0.0.1") || existingLinks.has(url)) {
           continue;
         }
@@ -109,29 +108,27 @@
     }
   }
 
+  function replaceWithTrueURL(fakeURLStr, trueURLParam) {
+    let fakeURL = new URL(fakeURLStr);
+    let trueURL = fakeURL.searchParams.get(trueURLParam);
+    window.location.replace(trueURL);
+  }
+
   $(document).ready(function () {
     if (window.location.href.includes("http://t.cn/")) {
       window.location.replace($(".wrap .link").first().text());
     } else if (window.location.href.includes("https://www.jianshu.com/go-wild?")) {
-      let fakeURL = new URL(window.location.href);
-      let trueURL = fakeURL.searchParams.get("url");
-      window.location.replace(trueURL);
+      replaceWithTrueURL(window.location.href, "url");
     } else if (window.location.href.includes("https://link.zhihu.com/?target=") || window.location.href.includes("http://link.zhihu.com/?target=")) {
-      window.location.replace($(".content .link").first().text());
+      replaceWithTrueURL(window.location.href, "target");
     } else if (window.location.href.includes("https://www.douban.com/link2/?url=")) {
-      let fakeURL = new URL(window.location.href);
-      let trueURL = fakeURL.searchParams.get("url");
-      window.location.replace(trueURL);
+      replaceWithTrueURL(window.location.href, "url");
     } else if (window.location.href.includes("https://link.ld246.com/forward?goto=")) {
-      let fakeURL = new URL(window.location.href);
-      let trueURL = fakeURL.searchParams.get("goto");
-      window.location.replace(trueURL);
+      replaceWithTrueURL(window.location.href, "goto");
     } else if (window.location.href.includes("https://mp.weixin.qq.com/")) {
       enableURLs();
     } else if (window.location.href.includes("http://redir.yy.duowan.com/warning.php?url=")) {
-      let fakeURL = new URL(window.location.href);
-      let trueURL = fakeURL.searchParams.get("url");
-      window.location.replace(trueURL);
+      replaceWithTrueURL(window.location.href, "url");
     } else if (window.location.href.includes("https://weixin110.qq.com/cgi-bin/mmspamsupport-bin/newredirectconfirmcgi")) {
       window.location.replace($(".weui-msg__desc").first().text());
     }
