19fb206c0f7b2b196ef6073ca89d2a8d83a3f6b9
OldPanda <me@old-panda.com>
2023-04-09 17:56:50 -0700

    Upgrade to 1.5.1

--- userscript.js
+++ userscript.js
@@ -51,7 +51,7 @@
 // @match          https://www.yuque.com/r/goto?url=*
 // @match          https://xie.infoq.cn/link?target=*
 // @exclude        https://mp.weixin.qq.com/cgi-bin/*
-// @version        1.5.0
+// @version        1.5.1
 // @run-at         document-idle
 // @namespace      https://old-panda.com/
 // @require        https://cdn.staticfile.org/jquery/3.6.3/jquery.min.js
