c29a73d63c015aa0c437d1e891390f3cf5630327
OldPanda <me@old-panda.com>
2023-07-12 21:12:22 -0700

    Update weibo redirect url

--- userscript.js
+++ userscript.js
@@ -5,9 +5,9 @@
 // @match          http*://c.pc.qq.com/*
 // @match          http*://iphone.myzaker.com/zaker/link.php?*
 // @match          http*://link.zhihu.com/?*
+// @match          http*://t.cn/*
 // @match          http*://www.360doc.cn/outlink.html?url=*
 // @match          http://redir.yy.duowan.com/warning.php?url=*
-// @match          http://t.cn/*
 // @match          http://www.360doc.com/content/*
 // @match          https://afdian.net/link?target=*
 // @match          https://bbs.nga.cn/read.php?*
@@ -52,7 +52,7 @@
 // @match          https://www.yuque.com/r/goto?url=*
 // @match          https://xie.infoq.cn/link?target=*
 // @exclude        https://mp.weixin.qq.com/cgi-bin/*
-// @version        1.6.0
+// @version        1.6.1
 // @run-at         document-idle
 // @namespace      https://old-panda.com/
 // @require        https://cdn.staticfile.org/jquery/3.6.3/jquery.min.js
@@ -109,11 +109,11 @@ const fuckers = {
   uisdc: { match: 'https://link.uisdc.com/?redirect=', redirect: "redirect" },
   wechat1: { match: 'https://mp.weixin.qq.com/s/', redirect: enableURLs },
   wechat2: { match: 'https://weixin110.qq.com/cgi-bin/mmspamsupport-bin/newredirectconfirmcgi', redirect: function () { window.location.replace($(".weui-msg__desc").first().text()) } },
-  // http://t.cn/RgAKoPE
+  // https://t.cn/RgAKoPE
   // https://weibo.cn/sinaurl?luicode=10000011&lfid=230259&u=http%3A%2F%2Ft.cn%2FA6qHeVlf
   // https://weibo.cn/sinaurl?toasturl=https%3A%2F%2Ftime.geekbang.org%2F
   // https://weibo.cn/sinaurl?u=https%3A%2F%2Fwww.freebsd.org%2F
-  weibo_1: { match: 'http://t.cn/', redirect: function () { const link = $(".wrap .link").first().text() || document.querySelector('.open-url').children[0].href; window.location.replace(link); } }, // 微博网页版
+  weibo_1: { match: 'https://t.cn/', redirect: function () { const link = $(".wrap .link").first().text() || document.querySelector('.open-url').children[0].href; window.location.replace(link); } }, // 微博网页版
   weibo_2: { match: 'https://weibo.cn/sinaurl?u', redirect: "u" },
   weibo_3: { match: 'https://weibo.cn/sinaurl?toasturl', redirect: "toasturl" },
   weibo_4: { match: 'https://weibo.cn/sinaurl?', redirect: function () { const link = $(".wrap .link").first().text() || document.querySelector('.open-url').children[0].href; window.location.replace(link); } },
