5186d9f9366e299c11b8fe5a051cc17ed5f0a62b
OldPanda <me@old-panda.com>
2023-12-08 09:45:45 -0800

    bump version to 1.7.1

--- userscript.js
+++ userscript.js
@@ -54,7 +54,7 @@
 // @match          https://www.yuque.com/r/goto?url=*
 // @match          https://xie.infoq.cn/link?target=*
 // @exclude        https://mp.weixin.qq.com/cgi-bin/*
-// @version        1.7.0
+// @version        1.7.1
 // @run-at         document-idle
 // @namespace      https://old-panda.com/
 // @require        https://cdn.staticfile.org/jquery/3.7.1/jquery.min.js
