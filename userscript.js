c4d21c543f8a999be4c9ee05fb94c6f8b42ef92a
OldPanda <me@old-panda.com>
2024-04-23 22:36:38 -0700

    bump version to 1.10.0

--- userscript.js
+++ userscript.js
@@ -58,7 +58,7 @@
 // @match          https://www.baike.com/redirect_link?url=*
 // @match          https://www.youtube.com/redirect?*
 // @exclude        https://mp.weixin.qq.com/cgi-bin/*
-// @version        1.9.0
+// @version        1.10.0
 // @run-at         document-idle
 // @namespace      https://old-panda.com/
 // @require        https://cdn.staticfile.org/jquery/3.7.1/jquery.min.js
