ef9f55dda9874821952dbe8695f32f4f4fa331ef
天边的生活 <104612354+tianbiandeshenghuo@users.noreply.github.com>
2025-02-08 08:41:47 +0800

    add luogu.com.cn/article

--- userscript.js
+++ userscript.js
@@ -62,6 +62,7 @@
 // @match          https://www.linkedin.com/safety/go?url=*
 // @match          https://www.luogu.com.cn/discuss/*
 // @match          https://www.luogu.com.cn/paste/*
+// @match          https://www.luogu.com.cn/article/*
 // @match          https://www.nodeseek.com/jump?to=*
 // @match          https://www.oschina.net/action/GoToLink?url=*
 // @match          https://www.pixiv.net/jump.php?*
@@ -119,6 +120,7 @@ const fuckers = {
   logonews: { match: 'https://link.logonews.cn/?', redirect: "url" },
   luogu: { match: 'https://www.luogu.com.cn/paste/', redirect: function () { if (document.getElementById("url")) { window.location.href = $("#url").text() } } },
   luogu_2: { match: 'https://www.luogu.com.cn/discuss/', redirect: function () { if (document.getElementById("url")) { window.location.href = $("#url").text() } } },
+  luogu_3: { match: 'https://www.luogu.com.cn/article/', redirect: function () { if (document.getElementById("url")) { window.location.href = $("#url").text() } } },
   nga: { match: 'https://nga.178.com/read.php?', redirect: function () { $("#m_posts #m_posts_c a").prop("onclick", null).off("click") } },
   nga2: { match: 'https://bbs.nga.cn/read.php?', redirect: function () { $("#m_posts #m_posts_c a").prop("onclick", null).off("click") } },
   nodeseek: { match: 'https://www.nodeseek.com/jump?to=', redirect: "to" },
