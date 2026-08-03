638479e346a3e0c642364c3e33e77995821d0dd6
huanfei <41602338+huanfe1@users.noreply.github.com>
2026-08-03 09:49:46 +0800

    bump version to 1.14.8

--- userscript.js
+++ userscript.js
@@ -96,7 +96,7 @@
 // @match          https://www.youtube.com/redirect?*
 // @match          https://wx.mail.qq.com/xmspamcheck/xmsafejump?*
 // @exclude        https://mp.weixin.qq.com/cgi-bin/*
-// @version        1.14.7
+// @version        1.14.8
 // @run-at         document-idle
 // @namespace      https://old-panda.com/
 // @require        https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js
